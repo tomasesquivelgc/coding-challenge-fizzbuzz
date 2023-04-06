431130a67ffc10a5b697bd5a6c051de88e2703c0
GitanoCosmico <117373513+tomasesquivelgc@users.noreply.github.com>
2023-04-06 13:59:53 -0300

    function fix
    now returns array

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,15 @@
 module.exports = function () {
-  for (var i = 1; i < 101; i++) {
-    if (i % 15 == 0) console.log("FizzBuzz");
-    else if (i % 3 == 0) console.log("Fizz");
-    else if (i % 5 == 0) console.log("Buzz");
-    else console.log(i);
+  let result = [];
+  for (let i = 1; i <= 100; i++){
+    if (i % 5 === 0 && i % 3 === 0){
+      result.push('FizzBuzz')
+    } else if (i % 3 === 0){
+      result.push('Fizz')
+    } else if (i % 5 === 0){
+      result.push('Buzz')
+    } else {
+      result.push(i)
+    }
   }
+  return result
 }
